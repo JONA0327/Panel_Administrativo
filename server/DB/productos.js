5e3c9b1464fe60382d85ac14420257689fcb908a
JONA0327 <jona03278@gmail.com>
2025-06-20 13:00:42 -0600

    Google Drive

---
+++ server/DB/productos.js
@@ -0,0 +1,17 @@
+// server/models/Product.js
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: String,
+  category: String,
+  suggestedInfo: String,
+  keywords: [String],
+  price: Number,
+  currency: String,
+  image: String
+});
+
+// El tercer parámetro fuerza el nombre de colección si lo necesitas:
+// module.exports = mongoose.model('Product', productSchema, 'mi_coleccion');
+
+module.exports = mongoose.model('Product', productSchema);
