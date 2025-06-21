6ee7e670b533817477a7ff4903446e86fe97bfb5
JONA0327 <jona03278@gmail.com>
2025-06-20 22:43:15 -0600

    Guardar Imagen en Drive

--- server/index.js
+++ server/index.js
@@ -112,7 +112,7 @@ app.delete('/products/:id', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
