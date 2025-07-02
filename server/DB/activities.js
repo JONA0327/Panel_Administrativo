f0269b23e8efb5c2604fcdb9ddf2ab5d10890ed0
JONA0327 <106278316+JONA0327@users.noreply.github.com>
2025-07-01 19:57:17 -0600

    Add user tracking to activity logs

--- server/DB/activities.js
+++ server/DB/activities.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const activitySchema = new mongoose.Schema({
   action: String,
   details: String,
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   createdAt: {
     type: Date,
     default: Date.now
